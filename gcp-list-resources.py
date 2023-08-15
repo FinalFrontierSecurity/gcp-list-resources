8e1907668d7561eaec0e68d4327ac5f37f166ae4
Michael Butler <michael@finalfrontiersecurity.com>
2023-08-14 22:07:57 -0400

    second commit

--- gcp-list-resources.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-
-subprocess.Popen(['gcloud','init'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-process = subprocess.Popen(['gcloud','asset','list','--project {0}'.format(project)], stdout=subprocess.PIPE, stderr=subprocess.PIPE))
-gcloud init
-gcloud asset list --project grounded-region-396000 --format="csv[]"
